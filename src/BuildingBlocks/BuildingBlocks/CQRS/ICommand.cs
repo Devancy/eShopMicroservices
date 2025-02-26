5c9f9d68d4a448d5c3a552428301d8e5d21ef93e
Diep Pham <diep.pham@niteco.se>
2025-02-26 18:03:21 +0700

    Catalog.API - CreateProduct p1

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+// No-response command, Unit is void in MediatR
+public interface ICommand : ICommand<Unit>
+{
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            //Id = Guid.NewGuid(), // TODO: remove this line later
+            Name = request.Name,
+            Categories = request.Categories,
+            Description = request.Description,
+            Price = request.Price
+        };
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+}
