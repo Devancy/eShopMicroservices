a210d998dfe9e3e54ce78746abfa0f87bcdf4564
Devancy <devancy@outlook.com.vn>
2025-02-27 23:21:36 +0700

    Catalog.API - Product - add missing ImageFile field

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Categories, string Description, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
 internal class CreateProductCommandHandler(IDocumentSession session)
