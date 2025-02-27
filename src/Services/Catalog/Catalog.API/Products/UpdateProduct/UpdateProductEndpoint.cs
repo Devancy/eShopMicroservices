34b99a1399f7e91d916e5ab4ccbaf4f4af6209b0
Devancy <devancy@outlook.com.vn>
2025-02-27 23:23:16 +0700

    Catalog.API - UpdateProduct

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+
+//public record class UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+            var result = await sender.Send(command);
+
+            //var response = result.Adapt<UpdateProductResponse>();
+            if (result is { IsSuccess: true })
+            {
+                return Results.NoContent();
+            }
+            return Results.NotFound();
+        })
+        .WithName("UpdateProduct")
+        .Produces<IResult>(StatusCodes.Status204NoContent)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
