d1e22483abe7426ea4adb95d29b8fa284893d1dd
Devancy <devancy@outlook.com.vn>
2025-03-03 23:33:23 +0700

    Basket.API - consume Discount.Grpc service

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc;
+
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -13,14 +15,26 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 	}
 }
 
-public class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProtoClient) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
 	public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
 	{
-		var cart = command.Cart;
-		await repository.StoreBasket(cart, cancellationToken);
+		await DeductDiscount(command.Cart, cancellationToken);
+		await repository.StoreBasket(command.Cart, cancellationToken);
 		// TODO: upsert the cache
 		
-		return new StoreBasketResult(cart.UserName, true);
+		return new StoreBasketResult(command.Cart.UserName, true);
+	}
+
+	private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+	{
+		foreach (var item in cart.Items)
+		{
+			var coupon = await discountProtoClient.GetDiscountAsync(new GetDiscountRequest
+			{
+				ProductName = item.ProductName,
+			}, cancellationToken: cancellationToken);
+			item.Price -= coupon.Amount;
+		}
 	}
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,41 +1,55 @@
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// App services
 var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddMediatR(config =>
 {
 	config.RegisterServicesFromAssembly(assembly);
 	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 	config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-builder.Services.AddValidatorsFromAssembly(assembly);
+
+// Data services
 builder.Services.AddMarten(opt =>
 {
 	opt.Connection(builder.Configuration.GetConnectionString("Database")!);
 	// Specify the ID column for ShoppingCart
 	opt.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
-// if (builder.Environment.IsDevelopment())
-// 	builder.Services.InitializeMartenWith<CatalogInitialData>();
-
-builder.Services.AddCarter();
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
-builder.Services.AddHealthChecks()
-	.AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
-	.AddRedis(builder.Configuration.GetConnectionString("Redis")!);
-
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
-
 builder.Services.AddStackExchangeRedisCache(option =>
 {
 	option.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
+
+// GRPC services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+	options.Address = new Uri(builder.Configuration["Grpc:DiscountAddress"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+	var handler = new HttpClientHandler
+	{
+		// this should be enabled in development env only
+		ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+	};
+	return handler;
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks()
+	.AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+	.AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
