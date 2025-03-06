01e95b006f0a99060c8c1859f50d22554abaa790
Devancy <devancy@outlook.com.vn>
2025-03-07 01:06:06 +0700

    Ordering - add sample data seeds

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,5 +13,10 @@ builder.Services.AddOrderingApplication()
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseOrderingApi();
+if (app.Environment.IsDevelopment())
+{
+	await app.InitializeDatabaseAsync();
+}
 
 app.Run();
