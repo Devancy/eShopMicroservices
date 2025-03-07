d19a482ee969f84a650de8497930c2820a79e46c
Devancy <devancy@outlook.com.vn>
2025-03-08 00:33:55 +0700

    Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,20 @@
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+	public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+	{
+		var pageIndex = query.PaginationRequest.PageIndex;
+		var pageSize = query.PaginationRequest.PageSize;
+		var total = await dbContext.Orders.LongCountAsync(cancellationToken);
+		
+		var orders = await dbContext.Orders
+			.Include(o => o.OrderItems)
+			.OrderBy(o => o.OrderName)
+			.Skip(pageSize * pageIndex)
+			.Take(pageSize)
+			.ToListAsync(cancellationToken);
+
+		return new GetOrdersResult(new PaginatedResult<OrderDto>(pageIndex, pageSize, total, orders.ToOrderDtoList()));
+	}
+}
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -15,7 +16,7 @@ public static class DependencyInjection
 			options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
 			options.UseSqlServer(configuration.GetConnectionString("Database"));
 		});
-		// services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+		services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 		
 		return services;
 	}
